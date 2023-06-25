430b489e418bc4eda012c270898f57422d0bb660
roto65 <antoniorotondo65@gmail.com>
2023-06-25 21:17:26 +0200

    Added JavaDocs for io.schemas package

--- src/main/java/io/schemas/HintSchema.java
+++ src/main/java/io/schemas/HintSchema.java
@@ -2,26 +2,51 @@ package io.schemas;
 
 import core.Move;
 
+/**
+ * Defines a schema for the hint collection inside the database
+ */
 public class HintSchema {
 
+    /**
+     * Disposition of the board for the given hint
+     */
     private String state;
+
+    /**
+     * Best move the player can do when the board corresponds to the state stored in the state string
+     */
     private Move bestMove;
 
+    /**
+     * Constructor called by the codec of this class
+     */
     public HintSchema() {
     }
 
+    /**
+     * @return the state associated with this hint
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * @return the best move that the player can do in the given state
+     */
     public Move getBestMove() {
         return bestMove;
     }
 
+    /**
+     * @param state new value that needs to replace the old one
+     */
     public void setState(String state) {
         this.state = state;
     }
 
+    /**
+     * @param bestMove new value that needs to replace the old one
+     */
     public void setBestMove(Move bestMove) {
         this.bestMove = bestMove;
     }
--- src/main/java/io/schemas/LevelSchema.java
+++ src/main/java/io/schemas/LevelSchema.java
@@ -5,25 +5,68 @@ import ui.blocks.Block;
 
 import java.util.ArrayList;
 
+/**
+ * Defines a schema for the level collection inside the database
+ */
 public class LevelSchema {
 
+    /**
+     * The number of the level, works as a primary key
+     */
     private Integer levelNumber;
+
+    /**
+     * The disposition of the blocks at the start of the level
+     */
     private ArrayList<Block> blocks;
+
+    /**
+     * The minimum moves that the player can do to win in the level
+     */
     private Integer minimumMoves;
+
+    /**
+     * The moves the player made before the schema is generated.
+     * Usually empty list in the database.
+     */
     private ArrayList<Move> moves;
+
+    /**
+     * The index of the move list that helps to keep track of undos the player has done
+     */
     private Integer iteratorIndex;
 
 
+
+    /**
+     * Empty constructor, called in parser
+     */
     @SuppressWarnings("unused")
     public LevelSchema() {
     }
 
+    /**
+     * Constructor used to create objects by the database adapter
+     *
+     * @param levelNumber the number of the level, works as a primary key
+     * @param blocks the disposition of the blocks at the start of the level
+     * @param minimumMoves the minimum moves that the player can do to win in the level
+     */
     public LevelSchema(Integer levelNumber, ArrayList<Block> blocks, Integer minimumMoves) {
         this.levelNumber = levelNumber;
         this.blocks = blocks;
         this.minimumMoves = minimumMoves;
     }
 
+    /**
+     * Constructor used to create objects by the board after it has been modified by the player
+     *
+     * @param levelNumber the number of the level, works as a primary key
+     * @param blocks the disposition of the blocks at the start of the level
+     * @param minimumMoves the minimum moves that the player can do to win in the level
+     * @param moves the moves the player made before the schema is generated
+     * @param iteratorIndex the index of the move list that helps to keep track of undos the player has done
+     */
     public LevelSchema(Integer levelNumber, ArrayList<Block> blocks, Integer minimumMoves, ArrayList<Move> moves, Integer iteratorIndex) {
         this.levelNumber = levelNumber;
         this.blocks = blocks;
@@ -32,22 +75,37 @@ public class LevelSchema {
         this.iteratorIndex = iteratorIndex;
     }
 
+    /**
+     * @return the number of the level, works as a primary key
+     */
     public int getLevelNumber() {
         return levelNumber;
     }
 
+    /**
+     * @return the disposition of the blocks at the start of the level
+     */
     public ArrayList<Block> getBlocks() {
         return blocks;
     }
 
+    /**
+     * @return the minimum moves that the player can do to win in the level
+     */
     public int getMinimumMoves() {
         return minimumMoves;
     }
 
+    /**
+     * @return the moves the player made before the schema is generated
+     */
     public ArrayList<Move> getMoves() {
         return moves;
     }
 
+    /**
+     * @return the index of the move list that helps to keep track of undos the player has done
+     */
     public int getIteratorIndex() {
         return iteratorIndex;
     }
