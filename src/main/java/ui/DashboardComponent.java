0f4efd157229172a839bbf13947e19175804d68f
roto65 <antoniorotondo65@gmail.com>
2023-06-25 11:25:48 +0200

    Added game title
    Somehow we were missing a title inside the game window, now there is one

--- src/main/java/ui/DashboardComponent.java
+++ src/main/java/ui/DashboardComponent.java
@@ -2,9 +2,12 @@ package ui;
 
 import core.StyledButton;
 import core.StyledLabel;
+import io.IOUtils;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static main.Constants.*;
@@ -14,32 +17,55 @@ public class DashboardComponent extends JPanel {
     public DashboardComponent(ArrayList<StyledButton> buttons, ArrayList<StyledLabel> labels) {
 
         setPreferredSize(new Dimension(TITLE_SIZE * 3, TITLE_SIZE * ROWS));
-
         setBackground(COLOR_BACKGROUND);
+        setLayout(new BorderLayout());
 
-        this.setLayout(new GridBagLayout());
+        int borderSize = TITLE_SIZE / 8;
+        setBorder(new EmptyBorder(new Insets(borderSize, borderSize, 0, borderSize)));
+
+
+        Font labelFont;
+        try {
+            labelFont = IOUtils.readFromTtf("DotGothic.ttf");
+        } catch (IOException | FontFormatException e) {
+            System.out.println("Error opening font file: " + e.getMessage());
+            return;
+        }
+
+        JLabel title = new JLabel(GAME_NAME);
+        title.setBackground(COLOR_BACKGROUND);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setForeground(Color.WHITE);
+        title.setFont(labelFont.deriveFont(Font.BOLD, (float) TITLE_SIZE / 3));
+
+        JPanel gridWrapper = new JPanel(new GridBagLayout());
+        gridWrapper.setBackground(COLOR_BACKGROUND);
+
         GridBagConstraints constraints = new GridBagConstraints();
 
         // padding settings to put some space between the buttons
-        constraints.insets = new Insets(TITLE_SIZE / 16, TITLE_SIZE / 8, TITLE_SIZE / 16, TITLE_SIZE / 8);
+        constraints.insets = new Insets(0, TITLE_SIZE / 8, TITLE_SIZE / 8, TITLE_SIZE / 8);
 
         for (StyledButton button : buttons) {
 
             constraints.gridx = button.getPos().x;
             constraints.gridy = button.getPos().y;
 
-            add(button, constraints);
+            gridWrapper.add(button, constraints);
         }
 
-        constraints.insets = new Insets(TITLE_SIZE / 16, 0, 0, 0);
+        constraints.insets = new Insets(0, 0, 0, 0);
 
         for (StyledLabel label : labels) {
 
             constraints.gridx = label.getPos().x;
             constraints.gridy = label.getPos().y;
 
-            add(label, constraints);
+            gridWrapper.add(label, constraints);
         }
+
+        add(title, BorderLayout.NORTH);
+        add(gridWrapper, BorderLayout.CENTER);
 
     }
 
